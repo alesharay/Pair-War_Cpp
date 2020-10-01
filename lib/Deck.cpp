c9dc4ccf23e0160cccd04e4d226307af7ee5b1e6
Alesha Ray <alesha.ray@g.austincc.edu>
2020-10-01 06:18:34 -0500

    update .gitignore to allow contents of lib/ to be pushed

---
+++ lib/Deck.cpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <iostream>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <fstream>
+
+#include "Deck.h"
+
+using namespace std;
+
+// <<<<<<<< LOADDECK >>>>>>>>
+void Deck::loadDeck() {
+  for(int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 13; ++j) {
+      tableCards[(i*13)+j] = j+1;
+    }
+  }
+} // loadDeck
+
+// <<<<<<<< SIZE >>>>>>>>
+int Deck::size() {
+  return sizeof(tableCards)/sizeof(tableCards[0]);
+} // end size
+
+// <<<<<<<< SHUFFLECARDS >>>>>>>>
+void Deck::shuffleCards() {
+  for( int i = 0; i < size()-1; ++i ) {
+    int randomIndex = i + (rand() % (size() - i));
+    int temp = tableCards[i];
+    tableCards[i] = tableCards[randomIndex];
+    tableCards[randomIndex] = temp;
+  }
+} // end shuffleCards
+
+// <<<<<<<< PUSH >>>>>>>>
+void Deck::push(int card) {
+  tableCards[size()-1] = card;
+} // end push
+
+// <<<<<<<< POP >>>>>>>>
+int Deck::pop() {
+  int card = tableCards[0];
+
+  for( int i = 0; i < size()-1; ++i ) {
+    tableCards[i] = tableCards[i+1];
+  }
+  tableCards[size()-1] = -1;
+
+  return card;
+} // end pop
+
+   
+// <<<<<<<< SHOWDECK >>>>>>>>
+void Deck::showDeck() {
+  printf("DECK: ");
+  fprintf(pFile, "DECK: ");   
+
+  for( int i = 0; i < size(); ++i) {
+    if( tableCards[i] == -1 ) {
+      printf("\n");
+      fprintf(pFile, "\n");
+      break;
+    } else {
+      printf( "%d ", tableCards[i] );
+      fprintf( pFile, "%d ", tableCards[i] );
+    }
+  }
+} // end showDeck
---
+++ lib/globals.cpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>         
+#include <iostream>
+
+#include "globals.h"
+
+#define NUM_THREADS 3      // number of player threads 
+#define NUM_CARDS 52       // number of cards
+
+// global variables ============================================================
+// THREAD g-vars:
+ pthread_mutex_t mutex_useDeck = PTHREAD_MUTEX_INITIALIZER;
+ pthread_cond_t condition_var = PTHREAD_COND_INITIALIZER;
+ pthread_mutex_t mutex_dealerExit = PTHREAD_MUTEX_INITIALIZER;
+ pthread_cond_t cond_win1 = PTHREAD_COND_INITIALIZER;
+ pthread_t playerThreads[NUM_THREADS];
+ pthread_t dealerThread;
+
+// OTHER g-vars:
+ FILE * pFile;              // point to ext file for the log
+ int roundNum = 1;          // keep track of which round it is
+ int numberOfRounds = 3;    // select how many rounds to play
+ int some_value = 1;        // TESTING ONLYxxxxxxxxxxxxxxxxxxxxxxx  
+ int turn = 0;              // keep track of whose turn it is
+ //int const NUM_CARDS = 52;  // number of cards in the deck 
+ int deck[NUM_CARDS];       // a card deck for shuffling and dealing
+ int *topOfDeck;            // point to top of deck
+ int *bottomOfDeck;         // point to bottom of deck
+ int seed = 0;              // the seed for rand() passed from command line   
+ bool win = false;          // flag to indicate a player has won
