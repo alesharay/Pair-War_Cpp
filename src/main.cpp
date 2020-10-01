320a1144ec471235555b725611dc2a3b8c1d6d10
Alesha Ray <alesha.ray@g.austincc.edu>
2020-10-01 01:10:58 -0500

    clean up file a bit; need to work on deck push and pop functions and move deal cards to dealer

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,3 @@
-/*******************************************************************************
-*   course:       CS4328 
-*   project:      Project 2 - Pair War 
-*   programmer:   Stacy Bridges
-*   due:          12/03/2015  
-*   submitted:    12/05/2015
-*   description:  Pair War Card Game
-*******************************************************************************/
-
-
 // library links ===============================================================
 #include "Deck.h"
 #include <pthread.h>
@@ -16,27 +6,6 @@
 #include <fstream>         // for writing to the log 
 #include <iostream>
 
-// // global variables ============================================================
-// // THREAD g-vars:
-// pthread_mutex_t mutex_useDeck = PTHREAD_MUTEX_INITIALIZER;
-// pthread_cond_t condition_var = PTHREAD_COND_INITIALIZER;
-// pthread_mutex_t mutex_dealerExit = PTHREAD_MUTEX_INITIALIZER;
-// pthread_cond_t cond_win1 = PTHREAD_COND_INITIALIZER;
-// pthread_t playerThreads[NUM_THREADS];
-// pthread_t dealerThread;
-
-// // OTHER g-vars:
-// FILE * pFile;              // point to ext file for the log
-// int roundNum = 1;          // keep track of which round it is
-// int numberOfRounds = 3;    // select how many rounds to play
-// int some_value = 1;        // TESTING ONLYxxxxxxxxxxxxxxxxxxxxxxx  
-// int turn = 0;              // keep track of whose turn it is
-// const int NUM_CARDS = 52;  // number of cards in the deck 
-// int deck[NUM_CARDS];       // a card deck for shuffling and dealing
-// int *topOfDeck;            // point to top of deck
-// int *bottomOfDeck;         // point to bottom of deck
-// int seed = 0;              // the seed for rand() passed from command line   
-// bool win = false;          // flag to indicate a player has won
 struct hand{               // a hand container for each player 
    int card1, card2;
 };
@@ -49,13 +18,9 @@ void *dealer_thread(void *arg);
 void *player_threads(void *playerId);
 void useTheDeck(long, hand);
 void randSeed();
-void buildDeck(); //-------------------loadDeck()
-void shuffleDeck(); //-----------------shuffleCards()
 void dealCards(); //-------------------dealCards()
 void playRound();
 void parseArgs(char *[]);
-void printDeck();
-
 
 // main ========================================================================
 int main(int argc, char *argv[]){
@@ -63,11 +28,9 @@ int main(int argc, char *argv[]){
    pFile = fopen("log.txt", "a"); // open the log file 
    parseArgs(argv);               // parse args from console   
    randSeed();                    // seed rand() with arg from command line
-   buildDeck();                   // populate the card deck
-  // cards.loadDeck();               // populate the card deck
+  cards.loadDeck();               // populate the card deck
 
-   while( roundNum <= 
-          numberOfRounds ){
+   while( roundNum <= numberOfRounds ){
       playRound();                // launch a round
       roundNum++;                 // inc the round counter to next round
       win = false;                // reset the win flag 
@@ -89,23 +52,9 @@ void randSeed(){ // seed rand() using arg x from the command line
    srand(seed);
 } // end function
 
-
-void buildDeck(){ // put 4 of each card value into deck[] where range is 0-12
-   int cardVal = 0, numInSuit = 4, card = 0;
-   while( (card < NUM_CARDS) && 
-          (cardVal < (NUM_CARDS/numInSuit)) ){
-      for( int i = 0; i < numInSuit; i++ ){
-         deck[card] = cardVal;
-         card++;
-      }
-      cardVal++;
-   }    
-   topOfDeck = deck;          // point to top of deck
-   bottomOfDeck = deck + 51;  // point to bottom of deck
-} // end function
-
-
 void playRound(){ // launch dealer and player threads for the current round
+   cards.showDeck();
+   cout << endl;
 
    printf("ROUND: %d ................\n", roundNum);
    fprintf(pFile, "ROUND: %d ................\n", roundNum);
@@ -190,9 +139,7 @@ void *player_threads(void *playerId){ // this function is for player threads
 
 void useTheDeck(long pId, hand thisHand){
    if( pId == 0 ){ // dealer uses the deck......................................   
-      fprintf(pFile, "DEALER: shuffle\n"); shuffleDeck(); // shuffle the deck    
-
-      // fprintf(pFile, "DEALER: shuffle\n"); cards.shuffleCards(); // shuffle the deck    
+      fprintf(pFile, "DEALER: shuffle\n"); cards.shuffleCards(); // shuffle the deck    
 
       fprintf(pFile, "DEALER: deal\n"); dealCards();      // deal the cards
    }    
@@ -202,10 +149,10 @@ void useTheDeck(long pId, hand thisHand){
       fprintf(pFile, "PLAYER %ld: hand %d \n", pId, thisHand.card1);
       
       // draw a card
-      thisHand.card2 = *topOfDeck, 
-      topOfDeck = topOfDeck + 1;
+      // thisHand.card2 = *topOfDeck, 
+      // topOfDeck = topOfDeck + 1;
 
-      // thisHand.card2 = cards.pop();
+      thisHand.card2 = cards.pop();
 
       fprintf(pFile, "PLAYER %ld: draws %d \n", pId,thisHand.card2); 
       
@@ -224,28 +171,30 @@ void useTheDeck(long pId, hand thisHand){
       else{
          // if the cards don't match, then discard a card            
          printf("WIN no\n");    
-         // shift cards in deck to make room for discard
 
-         topOfDeck = topOfDeck - 1;       
-         int *ptr = topOfDeck;
-         while( ptr != bottomOfDeck ){
-            *ptr = *(ptr + 1);
-            ptr = ptr + 1;      
-         }         
+         // shift cards in deck to make room for discard
+        //  topOfDeck = topOfDeck - 1;       
+        //  int *ptr = topOfDeck;
+        //  while( ptr != bottomOfDeck ){
+        //     *ptr = *(ptr + 1);
+        //     ptr = ptr + 1;      
+        //  }         
 
          // randomly select discard and put it back in the deck
-         int discard = rand()%2;
-         if( discard == 0 ){
+        //  int discard = rand()%2;
+         if( rand()%2 ){
             fprintf(pFile, "PLAYER %ld: discards %d \n", pId, thisHand.card1);   
-            *bottomOfDeck = thisHand.card1;  // put card back in deck
+
+            // *bottomOfDeck = thisHand.card1;  // put card back in deck
+            cards.push(thisHand.card1);
             thisHand.card1 = thisHand.card2; // set card1 to remaining card value
          }     
          else{
             fprintf(pFile, "PLAYER %ld: discards %d \n", pId, thisHand.card2);             
-            *bottomOfDeck = thisHand.card2;
+            // *bottomOfDeck = thisHand.card2;
          }   
          // print the contents of the deck
-         printDeck();                 
+        cards.showDeck();                
       }      
    }  
    turn ++; // inc turn so next player may use the deck
@@ -254,39 +203,13 @@ void useTheDeck(long pId, hand thisHand){
 } // end function
 
 
-void shuffleDeck(){ // dealer swaps current card w/ rand card til all shuffled
-   for( int i = 0; i < (NUM_CARDS - 1); i++ ){
-      int randPos = i + (rand() % (NUM_CARDS - i));  
-      int temp = deck[i];
-      deck[i] = deck[randPos]; 
-      deck[randPos] = temp;
-   }
-} // end function
-
-
 void dealCards(){ // the dealer deals one card into each hand
-   hand1.card1 = *topOfDeck; topOfDeck = topOfDeck + 1; 
-   hand2.card1 = *topOfDeck; topOfDeck = topOfDeck + 1;  
-   hand3.card1 = *topOfDeck; topOfDeck = topOfDeck + 1;   
+  //  hand1.card1 = *topOfDeck; topOfDeck = topOfDeck + 1; 
+  //  hand2.card1 = *topOfDeck; topOfDeck = topOfDeck + 1;  
+  //  hand3.card1 = *topOfDeck; topOfDeck = topOfDeck + 1;   
 
 
-  // hand1.card1 = cards.pop();      
-  // hand2.card1 = cards.pop();      
-  // hand3.card1 = cards.pop();      
+  hand1.card1 = cards.pop();      
+  hand2.card1 = cards.pop();      
+  hand3.card1 = cards.pop();      
 } // end function
-
-
-void printDeck(){ // print deck to console and to log
-   printf("DECK: ");
-   fprintf(pFile, "DECK: ");    
-   int *ptr = topOfDeck;
-   while( ptr != bottomOfDeck ){
-      printf("%d ", *ptr);
-      fprintf(pFile, "%d ", *ptr);
-      ptr++;
-      if( ptr == bottomOfDeck ){
-         printf("%d \n", *ptr);
-         fprintf(pFile, "%d \n", *ptr);     
-      }      
-   }
-}
